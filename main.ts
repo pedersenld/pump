4c21874acf491334b7e832f66d513c0565e03f2a
Lance <lance.pedersen@epsb.ca>
2023-02-15 17:29:59 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
+    cakLandPump.startDuration(cakLand.BoardSide.RIGHT, 60, 4)
+    basic.clearScreen()
 })
 basic.forever(function () {
 	
