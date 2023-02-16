1b608f9a9821c061ff642cd714ed38a1ee2ddcfe
Lance <lance.pedersen@epsb.ca>
2023-02-15 18:02:15 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
-    cakLandPump.startDuration(cakLand.BoardSide.RIGHT, 60, 4)
+    cakLandPump.startDuration(cakLand.BoardSide.RIGHT, 60, 1)
     basic.clearScreen()
 })
+basic.showIcon(IconNames.Heart)
+cakLandSoil.displayMoisture(cakLandSoil.SoilPin.P0)
+basic.pause(1000)
 basic.forever(function () {
-	
+    if (cakLandSoil.ifMoisture(cakLandSoil.SoilPin.P0, cakLandSoil.Mlevel.DRY)) {
+        cakLandPump.startDuration(cakLand.BoardSide.RIGHT, 60, 2)
+    }
 })
