0b9141d2fef3af03dda2319c1bdefd89ce5ea5f1
Lance <lance.pedersen@epsb.ca>
2023-03-13 10:58:59 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,30 @@
 input.onButtonPressed(Button.A, function () {
+    basic.showNumber(howManyWaterings)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Yes)
     cakLandPump.startDuration(cakLand.BoardSide.RIGHT, 60, 1)
     basic.clearScreen()
 })
+input.onButtonPressed(Button.B, function () {
+    cakLandSoil.displayMoisture(cakLandSoil.SoilPin.P0)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+let howManyWaterings = 0
 basic.showIcon(IconNames.Heart)
+howManyWaterings = 0
 cakLandSoil.displayMoisture(cakLandSoil.SoilPin.P0)
 basic.pause(1000)
 basic.forever(function () {
-    if (cakLandSoil.ifMoisture(cakLandSoil.SoilPin.P0, cakLandSoil.Mlevel.DRY)) {
-        cakLandPump.startDuration(cakLand.BoardSide.RIGHT, 60, 2)
+    if (cakLandSoil.ifMoisture(cakLandSoil.SoilPin.P0, cakLandSoil.Mlevel.VERY_DRY)) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Umbrella)
+        cakLandPump.startDuration(cakLand.BoardSide.RIGHT, 60, 4)
+        howManyWaterings += 1
+        basic.clearScreen()
     }
 })
